0fb8989f1e5e709e48f9d8e6d4f5a0e98a0a0439
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2026-05-18 11:36:46 +0200

    feat: proper sign out button

---
+++ src/app/(app)/profile/client-buttons.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ButtonGroup } from "@/components/ui/button-group";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTitle,
+  PopoverDescription
+} from "@/components/ui/popover";
+import { authClient } from "@/lib/auth-client";
+import { KeyRound, LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export default function ClientButtons() {
+  const router = useRouter();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/auth/signin");
+        }
+      }
+    });
+  }
+  return (
+    <ButtonGroup className="min-w-full">
+      <Button variant="outline">
+        <KeyRound />
+        Change Password
+      </Button>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button variant="outline">
+            <LogOut />
+            Sign Out
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-32">
+          <Button
+            variant="destructive"
+            onClick={() => signOut()}
+            className="w-full"
+          >
+            Are you sure?
+          </Button>
+        </PopoverContent>
+      </Popover>
+    </ButtonGroup>
+  );
+}
--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -4,27 +4,21 @@ import { checkSession } from "@/lib/utils-server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { EditProfileForm } from "./profile-form";
+import ClientButtons from "./client-buttons";
 
 export default async function Page() {
   const session = await checkSession();
   if (!session.success) redirect("/auth/signin");
 
-  async function signOut() {
-    "use server";
-    console.log("Signing out...");
-    await auth.api.signOut({ headers: await headers() });
-    redirect("/auth/signin");
-  }
-
   return (
     <div className="flex w-full items-center justify-center">
       <div className="w-full max-w-sm rounded-lg p-6 sm:border sm:bg-popover">
-        <EditProfileForm name={session.user.name} email={session.user.email} />
-        <form action={signOut}>
-          <Button type="submit" className="ml-4">
-            Sign out
-          </Button>
-        </form>
+        <EditProfileForm
+          name={session.user.name}
+          email={session.user.email}
+          className="mb-2"
+        />
+        <ClientButtons />
       </div>
     </div>
   );
