eb72b228f3929c522f2fb5f8086c90e9a9680014
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2026-06-08 22:06:36 +0200

    fix: make actions work on own vms

--- src/app/(app)/vms/[vm]/actions.ts
+++ src/app/(app)/vms/[vm]/actions.ts
@@ -19,111 +19,107 @@ export async function proxmoxVmAction(vmid: number, node: string, action: Key) {
   if (vm.userId !== session.data.user.id && !vm.allowActions)
     throw new Error("Action not allowed");
 
-  if (vm.shared)
-    switch (action) {
-      case "start":
-        const result = await proxmoxClient.nodes
-          .$(node)
-          .qemu.$(vmid)
-          .status.start.$post();
-        const waitedStart = await waitForTask(node, result);
-        if (!waitedStart.success) throw new Error(waitedStart.error);
-        await prisma.vm.update({
-          where: {
-            id: vmid
-          },
-          data: {
-            status: "running"
-          }
-        });
-        revalidatePath(`/vm/${vmid}`);
-        return waitedStart;
-      case "shutdown":
-        const shutdownResult = await proxmoxClient.nodes
-          .$(node)
-          .qemu.$(vmid)
-          .status.shutdown.$post();
-        const waitedShutdown = await waitForTask(node, shutdownResult);
-        if (!waitedShutdown.success) throw new Error(waitedShutdown.error);
-        await prisma.vm.update({
-          where: {
-            id: vmid
-          },
-          data: {
-            status: "stopped"
-          }
-        });
-        revalidatePath(`/vm/${vmid}`);
-        return waitedShutdown;
-      case "reboot":
-        const rebootResult = await proxmoxClient.nodes
-          .$(node)
-          .qemu.$(vmid)
-          .status.reboot.$post();
-        const waitedReboot = await waitForTask(node, rebootResult);
-        if (!waitedReboot.success) throw new Error(waitedReboot.error);
-        await prisma.vm.update({
-          where: {
-            id: vmid
-          },
-          data: {
-            status: "running"
-          }
-        });
-        revalidatePath(`/vm/${vmid}`);
-        return waitedReboot;
-      case "stop":
-        const stopResult = await proxmoxClient.nodes
-          .$(node)
-          .qemu.$(vmid)
-          .status.stop.$post();
-        const waitedStop = await waitForTask(node, stopResult);
-        if (!waitedStop.success) throw new Error(waitedStop.error);
-        await prisma.vm.update({
-          where: {
-            id: vmid
-          },
-          data: {
-            status: "stopped"
-          }
-        });
-        revalidatePath(`/vm/${vmid}`);
-        return waitedStop;
-      case "terminate":
-        if (vm.shared) throw new Error("Cannot terminate a shared VM");
-        const terminateStopResult = await proxmoxClient.nodes
-          .$(node)
-          .qemu.$(vmid)
-          .status.stop.$post();
-        const waitedStopTerminate = await waitForTask(
-          node,
-          terminateStopResult
-        );
-        if (!waitedStopTerminate.success)
-          throw new Error(waitedStopTerminate.error);
-        await prisma.vm.update({
-          where: {
-            id: vmid
-          },
-          data: {
-            status: "stopped"
-          }
-        });
-        revalidatePath(`/vm/${vmid}`);
-        const deleteResult = await proxmoxClient.nodes
-          .$(node)
-          .qemu.$(vmid)
-          .$delete({ "destroy-unreferenced-disks": true, purge: true });
-        const waitedDelete = await waitForTask(node, deleteResult);
-        if (!waitedDelete.success) throw new Error(waitedDelete.error);
-        await prisma.vm.delete({
-          where: {
-            id: vmid
-          }
-        });
-        revalidatePath("/");
-        redirect("/");
-    }
+  switch (action) {
+    case "start":
+      const result = await proxmoxClient.nodes
+        .$(node)
+        .qemu.$(vmid)
+        .status.start.$post();
+      const waitedStart = await waitForTask(node, result);
+      if (!waitedStart.success) throw new Error(waitedStart.error);
+      await prisma.vm.update({
+        where: {
+          id: vmid
+        },
+        data: {
+          status: "running"
+        }
+      });
+      revalidatePath(`/vm/${vmid}`);
+      return waitedStart;
+    case "shutdown":
+      const shutdownResult = await proxmoxClient.nodes
+        .$(node)
+        .qemu.$(vmid)
+        .status.shutdown.$post();
+      const waitedShutdown = await waitForTask(node, shutdownResult);
+      if (!waitedShutdown.success) throw new Error(waitedShutdown.error);
+      await prisma.vm.update({
+        where: {
+          id: vmid
+        },
+        data: {
+          status: "stopped"
+        }
+      });
+      revalidatePath(`/vm/${vmid}`);
+      return waitedShutdown;
+    case "reboot":
+      const rebootResult = await proxmoxClient.nodes
+        .$(node)
+        .qemu.$(vmid)
+        .status.reboot.$post();
+      const waitedReboot = await waitForTask(node, rebootResult);
+      if (!waitedReboot.success) throw new Error(waitedReboot.error);
+      await prisma.vm.update({
+        where: {
+          id: vmid
+        },
+        data: {
+          status: "running"
+        }
+      });
+      revalidatePath(`/vm/${vmid}`);
+      return waitedReboot;
+    case "stop":
+      const stopResult = await proxmoxClient.nodes
+        .$(node)
+        .qemu.$(vmid)
+        .status.stop.$post();
+      const waitedStop = await waitForTask(node, stopResult);
+      if (!waitedStop.success) throw new Error(waitedStop.error);
+      await prisma.vm.update({
+        where: {
+          id: vmid
+        },
+        data: {
+          status: "stopped"
+        }
+      });
+      revalidatePath(`/vm/${vmid}`);
+      return waitedStop;
+    case "terminate":
+      if (vm.shared) throw new Error("Cannot terminate a shared VM");
+      const terminateStopResult = await proxmoxClient.nodes
+        .$(node)
+        .qemu.$(vmid)
+        .status.stop.$post();
+      const waitedStopTerminate = await waitForTask(node, terminateStopResult);
+      if (!waitedStopTerminate.success)
+        throw new Error(waitedStopTerminate.error);
+      await prisma.vm.update({
+        where: {
+          id: vmid
+        },
+        data: {
+          status: "stopped"
+        }
+      });
+      revalidatePath(`/vm/${vmid}`);
+      const deleteResult = await proxmoxClient.nodes
+        .$(node)
+        .qemu.$(vmid)
+        .$delete({ "destroy-unreferenced-disks": true, purge: true });
+      const waitedDelete = await waitForTask(node, deleteResult);
+      if (!waitedDelete.success) throw new Error(waitedDelete.error);
+      await prisma.vm.delete({
+        where: {
+          id: vmid
+        }
+      });
+      revalidatePath("/");
+      redirect("/");
+  }
 }
 
 export async function inviteAction(
